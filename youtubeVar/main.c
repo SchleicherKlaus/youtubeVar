65e45d25c62b34b350ff257b688820fcc31bc219
Klaus Schleicher <klausschleicher@Klauss-MacBook-Air.local>
2013-06-19 21:29:03 +0200

    Initial Commit

---
+++ youtubeVar/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  youtubeVar
+//
+//  Created by Klaus Schleicher on 19.06.13.
+//  Copyright (c) 2013 Klaus Schleicher. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
